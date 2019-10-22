4d94d7724d15475f7ff3c9613d47c14d3c699e01
sarwar12 <sarwarkhan.nahin@gmail.com>
2019-10-22 11:19:44 +0600

    Staff Group Owl Carousel

---
+++ js/active.js
@@ -0,0 +1,10 @@
+$(document).ready(function($){
+	$(".staff-list").owlCarousel({
+		items:4,
+		autoplay:false,
+		margin:20,
+		loop:true,
+		nav:true,
+		navText:["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"],
+	});
+});
